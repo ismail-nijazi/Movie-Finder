ae5ea4dae8d970d1a119fc7e0fb5ab43c9a389a1
ismail-nijazi <ismail.nijazi1@gmail.com>
2023-01-28 14:17:43 +0100

    updated vecel file

--- Backend_MovieFinder/settings.py
+++ Backend_MovieFinder/settings.py
@@ -23,11 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     # --- Used in production --- 
-    'https://movie-finder-ivory.vercel.app/'
+    'https://movie-finder-ivory.vercel.app/',
+    '127.0.0.1'
 ]
 
 
