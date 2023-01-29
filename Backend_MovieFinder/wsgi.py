365581a699d6c9d2206053b645cdb2a2e9bfb9f0
ismail-nijazi <ismail.nijazi1@gmail.com>
2023-01-29 09:58:35 +0100

    added build files

--- Backend_MovieFinder/wsgi.py
+++ Backend_MovieFinder/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Backend_MovieFinder.settings')
 
 application = get_wsgi_application()
+app = application
