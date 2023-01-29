c917c41743ed1f16c20f0151707f0184bf3d3f46
ismail-nijazi <ismail.nijazi1@gmail.com>
2023-01-29 11:25:40 +0100

    added build files

---
+++ build_filles.sh
@@ -0,0 +1,2 @@
+cd ~ && wget https://www.sqlite.org/2020/sqlite-autoconf-3320100.tar.gz && tar xvfz sqlite-autoconf-3320100.tar.gz && cd sqlite-autoconf-3320100 && ./configure && make && make install
+pip install -r requirements.txt
