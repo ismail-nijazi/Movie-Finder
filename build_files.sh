d7150f22c554c65899edbbbc038908db089f60f5
ismail-nijazi <ismail.nijazi1@gmail.com>
2023-01-29 11:43:30 +0100

    ..

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,7 @@
 # build_files.sh
-wget https://www.sqlite.org/2019/sqlite-autoconf-3280000.tar.gz
+cd ~ && wget https://www.sqlite.org/2019/sqlite-autoconf-3280000.tar.gz
 tar zxvf sqlite-autoconf-3280000.tar.gz
-./configure
+cd sqlite-autoconf-3280000
 make
 sudo make install
 pip install -r requirements.txt
