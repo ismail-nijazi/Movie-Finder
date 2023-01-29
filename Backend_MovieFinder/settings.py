cfac83b8a2745b9a355591aeabd1adf00f228465
ismail-nijazi <ismail.nijazi1@gmail.com>
2023-01-29 12:17:14 +0100

    ...

--- Backend_MovieFinder/settings.py
+++ Backend_MovieFinder/settings.py
@@ -94,10 +94,10 @@ WSGI_APPLICATION = 'Backend_MovieFinder.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
